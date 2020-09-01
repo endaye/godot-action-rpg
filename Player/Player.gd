ded2d079950d5801867c9dde3ae91f4a5db69477
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-09-01 11:03:10 +0800

    add input func in Player.gd

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,5 +3,13 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		position.x += 10 * detla
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+	input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_collide(velocity)
