92d14979339b0c12c3787223b7db0fd0759db1a8
Yuancheng Zhang <old.far.hot@gmail.com>
2020-10-02 15:37:00 +0800

    P11 | Melee attacks with Hurtboxes and Hitboxes

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_pressed("attack"):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		queue_free()
+
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
