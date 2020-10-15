03f427b08e00636c094a6e9edfc347f12be806dd
Yuancheng Zhang <old.far.hot@gmail.com>
2020-10-16 04:55:04 +0800

    P17 | Player Stats + Enemy Attack

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -20,6 +20,7 @@ var state = IDLE
 onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var PlayerDetectionZone = $PlayerDetectionZone
+onready var hurtbox = $Hurtbox
 
 func _ready():
 	print(stats.max_health)
@@ -55,7 +56,7 @@ func seek_player():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
-
+	hurtbox.create_hit_effect()
 
 func _on_Stats_no_health():
 	queue_free()
--- Overlap/Hurtbox.gd
+++ Overlap/Hurtbox.gd
@@ -1,13 +1,36 @@
 extends Area2D
 
-export(bool) var show_hit = true
-
 const HitEffect = preload("res://Effects/HItEffect.tscn")
 
-func _on_Hurtbox_area_entered(area):
-	if show_hit:
-		var effect = HitEffect.instance()
-		var main = get_tree().current_scene
-		main.add_child(effect)
-		effect.global_position = global_position
+var invincible = false setget set_invincible
+
+onready var timer = $Timer
+
+signal invinciblility_started
+signal invinciblility_ended
+
+func set_invincible(value):
+	invincible = value
+	if invincible == true:
+		emit_signal("invinciblility_started")
+	else:
+		emit_signal("invinciblility_ended")
 	
+func start_invincibility(duration):
+	self.invincible = true
+	timer.start(duration)
+	
+func create_hit_effect():
+	var effect = HitEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(effect)
+	effect.global_position = global_position
+	
+func _on_Timer_timeout():
+	self.invincible = false
+	
+func _on_Hurtbox_invinciblility_started():
+	set_deferred("monitorable", false)
+
+func _on_Hurtbox_invinciblility_ended():
+	monitorable = true
